406f81fba3fdaceec209d945da6693220c34dd11
Maryam Azimi <maryam622azimi@gmail.com>
2026-08-23 11:45:18 +0330

    initial database migration with alembic

---
+++ alembic/env.py
@@ -0,0 +1,53 @@
+import sys
+from logging.config import fileConfig
+from pathlib import Path
+
+from alembic import context
+
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+
+from src.database import Base, engine
+from src.settings import settings
+
+config = context.config
+
+config.set_main_option("sqlalchemy.url", settings.database_url)
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode."""
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode."""
+    connectable = engine
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
